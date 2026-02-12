27f81879228f1a5ad6e41909465a1868d313d2d9
rewine <luhongxu@deepin.org>
2026-02-12 10:33:33 +0800

    Fix Boost.Bind deprecation warnings with version compatibility
    Use conditional compilation to support both old and new Boost.Bind API:
    - Boost >= 1.73.0: Use boost/bind/bind.hpp
    - Boost < 1.73.0: Use boost/bind.hpp
    
    This approach maintains backward compatibility while fixing deprecation
    warnings in newer Boost versions.

--- src/core/search/ParallelMultiSearcher.cpp
+++ src/core/search/ParallelMultiSearcher.cpp
@@ -5,7 +5,12 @@
 /////////////////////////////////////////////////////////////////////////////
 
 #include "LuceneInc.h"
+#include <boost/version.hpp>
+#if BOOST_VERSION >= 107300  // Boost 1.73.0+
+#include <boost/bind/bind.hpp>
+#else
 #include <boost/bind.hpp>
+#endif
 #include <boost/bind/protect.hpp>
 #include "ParallelMultiSearcher.h"
 #include "_MultiSearcher.h"
